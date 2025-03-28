b6e3b76bb592fed6738ca77ac3b5f3584ad6e2b6
tettehseth67 <68742079+tettehseth67@users.noreply.github.com>
2025-03-28 18:13:55 -0400

    update: redesigned footer styles for improved visibility and accessibility

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-        <div id="footer">
+        <div id="footer" class="bg-light text-center text-lg-start mt-5 p-3 text-black" style="position:fixed;bottom:0;width:100%;left:0;">
             <?php echo 'Copyright (c)' . date('M-d-y'); ?>
         </div>
         </div>
